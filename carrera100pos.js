d86bdf1c69056f4abea4ba357f3265cf092f6c24
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-02-27 16:22:37 -0600

    Carrera entre dos pulgas
    Esta es mi versión final, pero me gustaría saber las posibles optimizaciones!!

--- carrera100pos.js
+++ carrera100pos.js
@@ -4,38 +4,34 @@ console.log("El ganador es",carrera());
 
 function carrera()
 {
-    var pulga1=0,pulga2=0,avance=0,ganador="";
+    var pulga1=0,pulga2=0,ganador="",pos;
 
-    while(pulga1<=100 && pulga2<=100)
+    while(pulga1<100 && pulga2<100)
     {
-        pos1=dado();
-        if(pos1==1)
-            avance=3;
-        else if(pos1==2 || pos1==3)
-            avance=1;
-        else 
-            avance=2;
-        pulga1+=pos1;
-        console.log("Avance de pulga 1: ",pulga1);
-
-        pos2=dado();
-        if(pos2==1)
-            avance=3;
-        else if(pos2==2 || pos2==3)
-            avance=1;
-        else 
-            avance=2;
-        pulga2+=pos2;
-        console.log("Avance de pulga 2: ",pulga2);
+        console.log("Avance de pulga 1: ",pulga1+=salta());
+        console.log("Avance de pulga 2: ",pulga2+=salta());
     }
 
-    if(pulga1>pulga2)
+    if(pulga1>=100 && pulga2>=100)
+        ganador="Empate";
+    else if(pulga1>pulga2)
         ganador="Pulga 1";
     else if(pulga2>pulga1)
         ganador="Pulga 2";
-    else
-        ganador="Empate";
+
     return ganador;
+}
+
+function salta()
+{
+    var avance, pos=dado();
+    if(pos==1)
+        avance=3;
+    else if(pos==2 || pos==3)
+        avance=1;
+    else 
+        avance=2;
+    return avance;
 }
 
 function dado()
