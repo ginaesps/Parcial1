36cc93d3491fc6dfe6ef483a86966ee8ee79a0e1
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-02 15:30:05 -0600

    Update extra-invertirVector.js
    Ya logré que se invierta sobre si mismo, si es que eso tiene sentido...
    Saludos! HBD

--- extra-invertirVector.js
+++ extra-invertirVector.js
@@ -3,11 +3,18 @@ var vec=new Array(10);
 for(var i=0;i<10;i++)
     vec[i]=Math.ceil(Math.random()*10);
 
-console.log("Vector: ",vec);
-console.log(invertir(vec));
+console.log("Vector",vec);
+console.log("Vector invertido",invertir(vec));
 
 function invertir(vector) 
 {
-    for(var j=9;j>=0;j--)
-        console.log(vector[j]);
+    var j,temp,k=9;
+    for(j=0;j<5;j++)
+    {
+        temp=vector[j];
+        vector[j]=vector[k];
+        vector[k]=temp;
+        k--;
+    }
+    return vector;
 }
