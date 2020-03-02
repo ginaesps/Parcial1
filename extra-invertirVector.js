c84dd20e2c742f4f7a49e6bef8827e2ff1103caa
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-01 20:24:35 -0600

    Update invertir vectores
    Respecto a voltear la imagen, esta era mi idea original pero no supe muy bien que hacer respecto al undefined de al final :(
    Por otro lado, me pregunto si habría una manera más ordenada de mostrar el vector invertido y como siempre, mejoras!
    Saludos

--- extra-invertirVector.js
+++ extra-invertirVector.js
@@ -4,15 +4,10 @@ for(var i=0;i<10;i++)
     vec[i]=Math.ceil(Math.random()*10);
 
 console.log("Vector: ",vec);
-console.log("Vector invertido: ",invertir(vec));
+console.log(invertir(vec));
 
-function invertir(vector)
+function invertir(vector) 
 {
-    var vec2=new Array(10),k=0;
     for(var j=9;j>=0;j--)
-    {
-        vec2[j]=vector[k];
-        k++;
-    }
-    return vec2;
+        console.log(vector[j]);
 }
