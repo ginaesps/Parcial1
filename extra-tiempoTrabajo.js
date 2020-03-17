91f8df83dded238fa10a3e1182c444cb2142b9fc
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-17 17:54:33 -0600

    Update extra-tiempoTrabajo.js

--- extra-tiempoTrabajo.js
+++ extra-tiempoTrabajo.js
@@ -1,3 +1,5 @@
+//Calcular tiempo que un trabajador duró en su jornada a partir de una hora de entrada y salida (h a 24, jornada<24h)
+
 class hora
 {
     constructor(h,m,s)
@@ -8,36 +10,48 @@ class hora
     }
 }
 
-let entra= new hora(2,20,30);
-let sale= new hora(4,10,20);
-let cuantoTiempo= new hora(0,0,0);
+let entra= new hora(2,20,30), sale= new hora(4,10,20),cuantoTiempo= new hora(0,0,0);
 var cont=0;
 
 cuantoTiempo.s=(60-entra.s+sale.s);
-if(cuantoTiempo.s>0)
-{
-    cont++;
-    if(cuantoTiempo.s>=60)
-    {
-        cuantoTiempo.s-=60;
-        cont++;
-    }   
-}
-
 cuantoTiempo.m=60-entra.m+sale.m;
-cuantoTiempo.m-=cont;
-cont=0;
-if(cuantoTiempo.m>0)
-{
-    cont++;
-    if(cuantoTiempo.m>=60)
-    {
-        cuantoTiempo.m-=60;
-        cont=1;
-    }
-}
-
+cuantoTiempo.m-=calcTiempo(cuantoTiempo.s);
 cuantoTiempo.h=sale.h-entra.h;
-cuantoTiempo.h-=cont;
+cuantoTiempo.h-=calcTiempo(cuantoTiempo.m);
 
 console.log("Duró",cuantoTiempo.h,"h",",",cuantoTiempo.m,",",cuantoTiempo.s);
+console.log(segundosAhms(3602),"función s-->hms");
+console.log("Función hms-->s",hmsAsegundos(3,4,56));
+
+function calcTiempo(valor)
+{
+    var cont=0;
+    if(valor>0)
+    {
+        cont++;
+        if(valor>=60)
+        {
+            valor-=60;
+            cont++;
+        }
+    }
+    return cont;
+}
+
+function hmsAsegundos(h,m,s)//debe regresar los segundos que han pasado desde las 00:00:00 hasta la hora dada a formato de 24
+{
+    var segundos;
+    segundos=h*3600+m*60+s;
+    return segundos;
+}
+
+function segundosAhms(s)//debe recibir un valor total de segundos e imprimir con console.log() las horas, minutos y segundos equivalentes
+{
+    var hr,min,seg;
+
+    hr=Math.floor(s/3600);
+    min=Math.floor((s-hr*3600)/60);
+    seg=s-(hr*3600+min*60);
+    console.log(hr,"h",min,"min",seg,"seg");
+    return ":)";
+}
