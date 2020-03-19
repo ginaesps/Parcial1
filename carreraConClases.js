c9095e9c1df01d4ea695cf350ab1892693a07a69
raghum76 <raghum76@ucol.mx>
2020-03-19 15:49:47 -0600

    Actualización de propiedades
    Se están añadiendo propiedades, algunas con valor y otras para que se ajusten después.

--- carreraConClases.js
+++ carreraConClases.js
@@ -4,6 +4,9 @@ class corredor{
         this.num=numero;
         this.distancia=distancia;
         this.lugar=lugarLlegada;
+        this.nacionalidad="Mexicano"; //quiero que todos sean mexicanos
+        This.tipoSangre=""; // esta propiedad la dejo pendiente, pero ya tendre la posibilidad de que si 
+                            // creo un corredor le puedo poner su tipo de sangre porque la propiedad existe
     }
 }
 
