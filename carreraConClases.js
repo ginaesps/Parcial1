8695ce1d49cc54349960de15bfcc38d57d7d83ce
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-19 15:45:12 -0600

    Recreación de la carrera
    Carrera de 100 posiciones con clases

---
+++ carreraConClases.js
@@ -0,0 +1,9 @@
+class corredor{
+    constructor(nombre,numero,distancia,lugarLlegada){
+        this.nombre=nombre;
+        this.num=numero;
+        this.distancia=distancia;
+        this.lugar=lugarLlegada;
+    }
+}
+
