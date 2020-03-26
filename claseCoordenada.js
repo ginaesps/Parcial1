69528f9e6e5d7b6c36fae6f9339fcddce3007a88
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-25 19:39:37 -0600

    Update claseCoordenada.js

--- claseCoordenada.js
+++ claseCoordenada.js
@@ -1,17 +1,25 @@
 class Punto{
     constructor(x,y){
+        Punto.contador=0;
+        this.cont=Punto.contador++;
         this.x=x;
         this.y=y;
     }
     mostrar(){
         console.log("("+this.x+","+this.y+")");
     }
-    distanciaDestino(x,y){
+    distanciaADestino(x,y){
         var d=Math.sqrt(Math.pow((x-this.x),2)+Math.pow((y-this.y),2)); 
+        return d;
+    }
+    distanciaAPunto(pDestino){
+        var d=Math.sqrt(Math.pow((pDestino.x-this.x),2)+Math.pow((pDestino.y-this.y),2)); 
         return d;
     }
 }
 
 var mipunto=new Punto(3,5);
-mipunto.mostrar();
+var destino=new Punto(4,7);
 
+console.log(mipunto)
+console.log(destino)
