496e6bbeb09125b665ebfe0c8a435464431a378f
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-26 22:31:17 -0600

    Update classFormulario.js

--- classFormulario.js
+++ classFormulario.js
@@ -1,7 +1,12 @@
-class Product{
+class Formulario{
     constructor(name,price,quantity){
         this.name=name;
         this.price=price;
         this.qty=quantity;
     }
 }
+
+const btn=document.getElementById("boton").addEventListener("click",function(){
+    let nombre=getElementById("name").value;
+    console.log("nombre"+nombre);
+})
