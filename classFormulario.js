94e0efac7dd20e33e94af26338964c902730c448
raghum76 <raghum76@ucol.mx>
2020-03-31 17:37:27 -0600

    Ajustes de clases
    uso de clases para diseño

--- classFormulario.js
+++ classFormulario.js
@@ -6,25 +6,21 @@ class Producto{
     }
 }
 
-class UI{ 
+class UI{ //UI: User Interface
     addProduct(product){
         const productList=document.getElementById('productList');
         const element=document.createElement('div');
         element.innerHTML =` 
             <div class='card text-center mb-4>
-            <div class='card-body'>
-            </div>
-            </div>
-            <div>
-                <div>
+                <div class='card-body'>
                     <strong>product Name</strong>: ${product.name} 
                     <strong>Price</strong>: ${product.price}
                     <strong>Quantity</strong>: ${product.qty}
                     <a href='#' class='btn btn-danger' name='delete'>Delete</a>
                 </div>
             </div>
-        `; 
-        productList.appendChild(element);
+        `; // ese tipo de apóstrofe es para indicar una instrucción de varias líneas
+        productList.appendChild(element); // INVESTIGAR
     }
     resetForm(){
         document.getElementById('product-form').reset();
@@ -37,6 +33,7 @@ const btn=document.getElementById("product-form").addEventListener('submit',(e)=
     const qty=document.getElementById('quantity').value;
     const price=document.getElementById('price').value;
     const product=new Producto(name,price,qty);
+    //aqui quite el href
     console.log(product);
     var ui=new UI();
     ui.addProduct(product);
