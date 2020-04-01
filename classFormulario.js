3380687dadb8cb9f375dfae528af08f3ea40d538
ginaesps <58644384+ginaesps@users.noreply.github.com>
2020-03-31 21:07:41 -0600

    Versión final para el formulario de Productos html

--- classFormulario.js
+++ classFormulario.js
@@ -6,24 +6,29 @@ class Producto{
     }
 }
 
-class UI{ //UI: User Interface
+class UI{ 
     addProduct(product){
         const productList=document.getElementById('productList');
         const element=document.createElement('div');
         element.innerHTML =` 
-            <div class='card text-center mb-4>
+            <div class='card text-center mb-4'>
                 <div class='card-body'>
-                    <strong>product Name</strong>: ${product.name} 
-                    <strong>Price</strong>: ${product.price}
-                    <strong>Quantity</strong>: ${product.qty}
+                    <strong>Name:</strong> ${product.name} 
+                    <strong>Price:</strong> ${product.price}
+                    <strong>Quantity:</strong> ${product.qty}
                     <a href='#' class='btn btn-danger' name='delete'>Delete</a>
                 </div>
             </div>
-        `; // ese tipo de apóstrofe es para indicar una instrucción de varias líneas
-        productList.appendChild(element); // INVESTIGAR
+        `; 
+        productList.appendChild(element);
     }
     resetForm(){
         document.getElementById('product-form').reset();
+    }
+    deleteProduct(element){
+        if(element.name==='delete'){
+            element.parentElement.parentElement.parentElement.remove()
+        }
     }
 }
 
@@ -33,10 +38,16 @@ const btn=document.getElementById("product-form").addEventListener('submit',(e)=
     const qty=document.getElementById('quantity').value;
     const price=document.getElementById('price').value;
     const product=new Producto(name,price,qty);
-    //aqui quite el href
     console.log(product);
     var ui=new UI();
     ui.addProduct(product);
     ui.resetForm();
     e.preventDefault();
+})
+
+const btnDelete=document.getElementById("productList").addEventListener('click',(e)=>{
+    if(e.target.name==='delete'){
+        var ui=new UI();
+        ui.deleteProduct(e.target);
+    }  
 })
